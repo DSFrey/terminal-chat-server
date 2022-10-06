b8aa05ebf95f5ba45516094a3a747bf2b9280213
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-10-06 22:39:52 +0000

    Write bare-bones server

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+require('dotenv').config();
+const PORT = process.env.PORT || 3002;
+const { Server } = require('socket.io');
+let server = new Server(PORT);
+
+server.on('connection', (socket)=>{
+  console.log(`Socket connected`, socket);
+
+  socket.on('JOIN', (room)=>{
+    socket.join(room);
+  });
+
+  socket.on('MESSAGE', (message)=>{
+    console.log(message);
+  });
+});
