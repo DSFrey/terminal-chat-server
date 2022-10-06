0dcd73904a0ca8ee4d6c788ade609d76de62d646
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-10-06 22:54:04 +0000

    Add response to message

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ const PORT = process.env.PORT || 3002;
 const { Server } = require('socket.io');
 let server = new Server(PORT);
 
-server.on('connection', (socket)=>{
+server.on('connection', (socket) => {
   console.log(`Socket connected`, socket);
 
-  socket.on('JOIN', (room)=>{
+  socket.on('JOIN', (room) => {
     socket.join(room);
-  });
+    socket.on('MESSAGE', (message) => {
+      console.log(message);
+      socket.to(room).emit('MESSAGE','Message received');
+    });
 
-  socket.on('MESSAGE', (message)=>{
-    console.log(message);
   });
 });
